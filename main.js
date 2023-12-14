7d7257668a57c85301727aff58e5bc2f56c45b32
Mateusuzumaki14 <122612365+Mateusuzumaki14@users.noreply.github.com>
2023-12-14 09:12:07 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var capImg='';
-var status=''
+var status='';
+var objects=[];
 function preload() {
     capImg=loadImage('dog_cat.jpg')
 }
@@ -20,18 +21,31 @@ function gotResult(error,results) {
         console.error(error);
     }
     console.log(results)
+    objects=results
 }
 function draw() {
     image(capImg,0,0,699,449);
-    fill('green');
-    text('dog',170,50); 
-    noFill();
-    stroke('red');
-    rect(160,30,180,350);
+    // fill('green');
+    // text('dog',170,50); 
+    // noFill();
+    // stroke('red');
+    // rect(160,30,180,350);
 
-    fill('blue');
-    text('cat',350,50);
-    noFill();
-    stroke('green');
-    rect(350,30,300,350);
+    // stroke('green');
+    // fill('blue');
+    // text('cat',350,50);
+    // noFill();
+   
+    // rect(350,30,300,350);
+    if (status!='') {
+        for (var i= 0;  i<objects.length; i++ ) {
+            document.getElementById('status').innerHTML='status: objeto detectado';
+            fill('yellow');
+            percent=floor(objects[i].confidence*100);
+            text(objects[i].label+''+percent+'%',objects[i].x,objects[i].y);
+            noFill();
+            stroke('black');
+            rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height)
+        }        
+    }
 }
